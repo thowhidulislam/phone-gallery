bb1966d76a6c1ce3790221e91dc797a14fa9ce32
thowhidulislam <thowhid.ndc14@gmail.com>
2022-03-02 15:23:08 +0600

    chaining done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+/* -------------------------------------
+    Displaying search result section
+---------------------------------------- */
+//fetching mobile data from API
 const loadPhones = () => {
     document.getElementById('no-result').textContent = '';
     document.getElementById('details-container').textContent = "";
@@ -8,15 +12,13 @@ const loadPhones = () => {
     fetch(url)
         .then(res => res.json())
         .then((data) => {
-            // console.log(data.data);
-
+            console.log(data.data);
             if (data.data.length == 0) {
                 const noResultContainer = document.getElementById('no-result');
                 const div = document.createElement('div');
                 div.style.textAlign = "center";
-                // div.style.marginTop = "center";
                 div.innerHTML = `
-               <p>No result found!</p>
+               <p class="text-danger">No result found!</p>
                `
                 noResultContainer.appendChild(div);
             }
@@ -27,6 +29,7 @@ const loadPhones = () => {
         })
 }
 
+//Displaying mobile phones on UI
 const displayPhones = phones => {
     const phone = phones.slice(0, 20);
     for (const phone of phones) {
@@ -34,19 +37,24 @@ const displayPhones = phones => {
         const div = document.createElement('div');
         div.classList.add('col-lg-4')
         div.innerHTML = `
-                <div class="border p-4 mt-4 rounded h-100">
+                <div class="border p-4 mt-4 rounded h-100 text-center">
                     <div id="mobile-container">
                         <img src="${phone.image}" alt="" id="mobile-image" class=" mb-4">
                     </div>
                         <h3>${phone.phone_name}</h3>
                         <h4>${phone.brand}</h4>
-                    <button onclick="loadDetails('${phone.slug}')" id="btn-details" class="p-2 w-75 mt-3">Details</button>
+                    <button onclick="loadDetails('${phone.slug}')" id="btn-details" class="p-2 w-75 mt-3 border-0 ">Details</button>
                 </div>
         `
         rowDiv.appendChild(div);
     }
 }
 
+/* ----------------------------------
+     Showing mobile details section
+------------------------------------- */
+
+// fetching details from API
 const loadDetails = id => {
     document.getElementById('details-container').textContent = '';
     const url = `https://openapi.programming-hero.com/api/phone/${id}`
@@ -54,7 +62,7 @@ const loadDetails = id => {
         .then(res => res.json())
         .then(data => showDetails(data.data))
 }
-
+// showing details on UI
 const showDetails = info => {
     console.log(info)
     const detailsContainer = document.getElementById('details-container');
@@ -63,24 +71,22 @@ const showDetails = info => {
 
     div.innerHTML = `
         <div class="card-body">
-            <div class=" p-3 mt-4 rounded h-100 w-100 m-auto d-sm-flex d-md-flex flex-md-column d-lg-flex flex-lg-row ">
+            <div class=" p-3 mt-4 rounded h-100 w-100 m-auto d-sm-flex d-md-flex d-lg-flex flex-sm-column  flex-lg-row ">
                 <div id="details-image-container">
-                    <img src="${info.image}" alt="" id="mobile-details-image" class=" mb-4">
+                    <img src="${info.image}" alt="" id="mobile-details-image" class=" mb-4 mx-auto d-block ">
                 </div>
-                <div class ="ms-sm-2 ms-md-3 ms-lg-5">
-                    <h3>${info.name}</h3>
-                    <h4>Brand name: ${info.brand}</h4>
-                    <h4 class="mt-3">Main Features</h4>
-                    <p>Storage:${info.mainFeatures.storage}</p>
-                    <p>Display Size:${info.mainFeatures.displaySize}</p>
+                <div class ="ms-sm-2 ms-md-1 ms-lg-5">
+                    <h3 class=" text-center text-sm-center text-lg-start">${info.name}</h3>
+                    <p class=" text-center text-sm-center text-lg-start">Release date: ${info.releaseDate ? info.releaseDate : 'No release date found'}</p>
+                    <h4 class="mt-3 text-center text-sm-center text-lg-start">Main Features</h4>
+                    <p>Storage: ${info.mainFeatures.storage}</p>
+                    <p>Display Size: ${info.mainFeatures.displaySize}</p>
                     <p>Chip set: ${info.mainFeatures.chipSet}</p>
                     <p>Sensors: ${info.mainFeatures.sensors}</p>
-                    <p>Others: USB: ${info.others.USB}, Bluetooth: ${info.others.Bluetooth}, WLAN: ${info.others.WLAN}, Radio: ${info.others.Radio}, NFC: ${info.others.NFC}, GPS: ${info.others.GPS}</p>
-                    <p>Release date: ${info.releaseDate}</p>
+                    <p>Others: USB: ${info.others ? info.others.USB : 'No'}, Bluetooth: ${info.others ? info.others.Bluetooth : 'No'}, WLAN: ${info.others ? info.others.WLAN : 'No'}, Radio: ${info.others ? info.others.Radio : 'No'}, NFC: ${info.others ? info.others.NFC : 'No'}, GPS: ${info.others ? info.others.GPS : 'No'}</p> 
                 </div>
             </div>
         </div>
     `
     detailsContainer.appendChild(div)
-
 }
